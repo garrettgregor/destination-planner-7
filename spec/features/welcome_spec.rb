97e37e3e1e4374fc85162701ef09d4b9fc1cb5aa
Garrett Gregor <garrett.gregor@gmail.com>
2024-01-30 11:46:12 -0700

    fix spacing rubocop errors

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -1,8 +1,8 @@
 require "rails_helper"
 
 RSpec.describe "Welcome", type: :feature do
-  let!(:destination1) { Destination.create(name: "Denver", zip: "80202", description: "Mile High City", image_url: "https://via.placeholder.com/300x300.png")}
-  let!(:destination2) { Destination.create(name: "New York", zip: "10001", description: "Big Apple", image_url: "https://via.placeholder.com/300x300.png")}
+  let!(:destination1) { Destination.create(name: "Denver", zip: "80202", description: "Mile High City", image_url: "https://via.placeholder.com/300x300.png") }
+  let!(:destination2) { Destination.create(name: "New York", zip: "10001", description: "Big Apple", image_url: "https://via.placeholder.com/300x300.png") }
 
   before(:each) do
     visit root_path
