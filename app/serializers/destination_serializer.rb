060059ef5e5893f59d9fe1f269f1a17730f94909
Garrett Gregor <garrett.gregor@gmail.com>
2024-02-09 16:40:09 -0700

    feat: add weather and gif url to destination show endpoint

--- app/serializers/destination_serializer.rb
+++ app/serializers/destination_serializer.rb
@@ -2,4 +2,28 @@ class DestinationSerializer
   include JSONAPI::Serializer
 
   attributes :name, :zip, :description, :image_url
+
+  def self.serialize_with_info(destination, weather, gif) # rubocop:disable Metrics/MethodLength
+    {
+      data: {
+        id: destination.id.to_s,
+        type: "destination",
+        attributes: {
+          name: destination.name,
+          description: destination.description,
+          zip: destination.zip,
+          image_url: destination.image_url
+        },
+        weather: {
+          current_temp: weather.current_temp.to_s,
+          max_temp: weather.max_temp.to_s,
+          min_temp: weather.min_temp.to_s,
+          summary: weather.summary
+        },
+        gif: {
+          gif_url: gif.url
+        }
+      }
+    }
+  end
 end
