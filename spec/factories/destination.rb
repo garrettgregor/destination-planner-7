c5d41712294642cf8c9e7d5900ffef166708a48b
Garrett Gregor <garrett.gregor@gmail.com>
2024-02-09 16:44:34 -0700

    fix rubocop errors

--- spec/factories/destination.rb
+++ spec/factories/destination.rb
@@ -1,7 +1,6 @@
 FactoryBot.define do
   factory :destination do
     # An array of zip codes representing different locations.
-    # rubocop:disable Layout/LineLength
     zips = %w[38016 70115 12203 30014 49418 11731 44133 48348 44646 15068 33801 08302 11432 43123 40165 33756 37013 32068 59901 02474] # rubocop:disable Layout/LineLength
 
     name { Faker::Address.city }
